87961275cabfd0357d97d059f80f093fa6d64823
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2024-01-11 15:50:02 +0700

    Latihan Coroutine Flow starter

--- Studi Kasus ReactiveX (RxJava) pada Aplikasi/a165-android-expert-project-retrofit/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiService.kt
+++ Studi Kasus ReactiveX (RxJava) pada Aplikasi/a165-android-expert-project-retrofit/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiService.kt
@@ -1,11 +1,12 @@
 package com.dicoding.tourismapp.core.data.source.remote.network
 
 import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
+import io.reactivex.Flowable
 
 import retrofit2.http.GET
 import retrofit2.Call
 
 interface ApiService {
     @GET("list")
-    fun getList(): Call<ListTourismResponse>
+    fun getList(): Flowable<ListTourismResponse>
 }
